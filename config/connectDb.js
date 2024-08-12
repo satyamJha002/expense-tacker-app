43b01724cf2e91c47feaaadb50203d865ea7b8b1
satyamJha002 <satyam073000@gmail.com>
2024-08-12 13:17:52 +0530

    expense-tracker-app

---
+++ config/connectDb.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDb = (req, res) => {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`${error}`.bgRed);
+  }
+};
+
+module.exports = connectDb;
